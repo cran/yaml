2070fb5682a19451750bd623e2ee92f3c27e6685
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2020-02-01 04:50:02 +0000

    version 2.2.1

--- R/yaml.load.R
+++ R/yaml.load.R
@@ -1,6 +1,13 @@
 `yaml.load` <-
-function(string, as.named.list = TRUE, handlers = NULL, error.label = NULL, eval.expr = getOption("yaml.eval.expr", TRUE)) {
-  eval.warning <- missing(eval.expr) && is.null(getOption("yaml.eval.expr"))
+function(string, as.named.list = TRUE, handlers = NULL, error.label = NULL,
+         eval.expr = getOption("yaml.eval.expr", TRUE),
+         merge.precedence = c("order", "override"), merge.warning = FALSE) {
+
   string <- enc2utf8(paste(string, collapse = "\n"))
-  .Call(C_unserialize_from_yaml, string, as.named.list, handlers, error.label, eval.expr, eval.warning, PACKAGE="yaml")
+  eval.warning <- missing(eval.expr) && is.null(getOption("yaml.eval.expr"))
+  merge.precedence <- match.arg(merge.precedence)
+
+  .Call(C_unserialize_from_yaml, string, as.named.list, handlers, error.label,
+        eval.expr, eval.warning, merge.precedence, merge.warning,
+        PACKAGE="yaml")
 }
